0293b8fd1d2af1d4a21451f29da8c375782817ba
AndreyBuyanov <andreew.12.87@gmail.com>
2021-05-27 21:50:39 +0700

    Add nn.py

---
+++ sane/nn.py
@@ -0,0 +1,23 @@
+from typing import List
+import numpy as np
+from neuron import Neuron
+from layer import Layer
+from activations import Sigmoid
+
+
+class NeuralNetwork(object):
+    def __init__(self, hidden_neurons: List[Neuron], inputs_count: int, outputs_count: int):
+        self.input_weights = np.zeros((len(hidden_neurons), inputs_count))
+        self.output_weights = np.zeros((outputs_count, len(hidden_neurons)))
+        for i in range(len(hidden_neurons)):
+            self.input_weights[i] = hidden_neurons[i].get_input_weights(inputs_count)
+            self.output_weights[i] = hidden_neurons[i].get_output_weights(outputs_count)
+        self.layers = []
+        self.layers.append(Layer(self.input_weights), Sigmoid())
+        self.layers.append(Layer(self.output_weights), Sigmoid())
+
+    def forward(self, input_data: np.array) -> np.array:
+        output = input_data
+        for layer in self.layers:
+            output = layer.forward(output)
+        return output
