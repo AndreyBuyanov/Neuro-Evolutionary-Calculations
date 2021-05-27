d907dcdef54540cade4ea45becd55f97eed6b085
AndreyBuyanov <andreew.12.87@gmail.com>
2021-05-27 21:43:27 +0700

    Add blueprint.py

---
+++ sane/blueprint.py
@@ -0,0 +1,12 @@
+from neuron import Neuron
+
+
+class Blueprint(object):
+    def __init__(self, neurons_count: int, connections_count: int):
+        self.neurons = [None] * neurons_count
+        self.connections_count = connections_count
+
+    def init(self, min_value: float, max_value: float):
+        for i in range(0, len(self.neurons)):
+            self.neurons[i] = Neuron(connections_count=self.connections_count)
+            self.neurons[i].init(min_value=min_value, max_value=max_value)
