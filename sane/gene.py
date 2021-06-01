6c7d38f87dda416e971f5ab773cbb8a767ec183a
AndreyBuyanov <andreew.12.87@gmail.com>
2021-06-01 11:52:30 +0700

    Add real gene

--- sane/gene.py
+++ sane/gene.py
@@ -11,12 +11,18 @@ class ConnectionType(Enum):
         return cls(cls.OUTPUT if label > (2**7 - 1) else cls.INPUT)
 
 
-def crossover(parent1: int, parent2: int, precision: int):
+def crossover_integer(parent1: int, parent2: int, precision: int):
     crossover_point = random.randrange(precision)
     mask1 = ((2 ** precision - 1) << crossover_point) & (2 ** precision - 1)
     mask2 = ((2 ** precision - 1) >> (precision - crossover_point)) & (2 ** precision - 1)
     child1 = (parent1 & mask1) | (parent2 & mask2)
     child2 = (parent2 & mask1) | (parent1 & mask2)
+    return child1, child2
+
+
+def crossover_real(parent1: float, parent2: float, blend=0.1):
+    child1 = parent1 - blend * (parent2 - parent1)
+    child2 = parent2 + blend * (parent2 - parent1)
     return child1, child2
 
 
@@ -51,7 +57,7 @@ class IntegerGene(object):
         min_value = parent1.min_value
         max_value = parent1.max_value
         precision = parent1.precision
-        child1_gene, child2_gene = crossover(
+        child1_gene, child2_gene = crossover_integer(
             parent1=parent1.int_value,
             parent2=parent2.int_value,
             precision=precision)
@@ -70,14 +76,48 @@ class IntegerGene(object):
         return child1, child2
 
 
+class RealGene(object):
+    def __init__(self, value: float, min_value: float, max_value: float):
+        self.value = value
+        self.min_value = min_value
+        self.max_value = max_value
+
+    def init(self):
+        self.value = random.uniform(self.min_value, self.max_value)
+
+    def mutation(self):
+        self.value = random.gauss(mu=self.value, sigma=0.1)
+
+    @staticmethod
+    def crossover(parent1, parent2):
+        min_value = parent1.min_value
+        max_value = parent1.max_value
+        child1_gene, child2_gene = crossover_real(
+            parent1=parent1.value,
+            parent2=parent2.value)
+        child1 = RealGene(
+            value=child1_gene,
+            min_value=min_value,
+            max_value=max_value)
+        child2 = RealGene(
+            value=child2_gene,
+            min_value=min_value,
+            max_value=max_value)
+        return child1, child2
+
+
 class Gene(object):
     def __init__(self, min_value: float, max_value: float):
         self.label = 0
-        self.weight = IntegerGene(
+        #self.weight = IntegerGene(
+        #    value=0.0,
+        #    min_value=min_value,
+        #    max_value=max_value,
+        #    precision=32)
+        self.weight = RealGene(
             value=0.0,
             min_value=min_value,
-            max_value=max_value,
-            precision=32)
+            max_value=max_value)
 
     def get_connection_type(self) -> ConnectionType:
         return ConnectionType.from_label(self.label)
@@ -108,11 +148,14 @@ class Gene(object):
         child2 = Gene(
             min_value=min_value,
             max_value=max_value)
-        child1.label, child2.label = crossover(
+        child1.label, child2.label = crossover_integer(
             parent1=parent1.label,
             parent2=parent2.label,
             precision=8)
-        child1.weight, child2.weight = IntegerGene.crossover(
+        #child1.weight, child2.weight = IntegerGene.crossover(
+        #    parent1=parent1.weight,
+        #    parent2=parent2.weight)
+        child1.weight, child2.weight = RealGene.crossover(
             parent1=parent1.weight,
             parent2=parent2.weight)
         return child1, child2
